05aadf7c336ab29c26c5d7d76d19c77a7bc72307
Carl Bennett <Admin@Jailout2000.com>
2011-11-25 01:33:44 -0600

    Removed broken debug messages (deoxxa broke them shortly afte I added them...)

--- src/mineserver/watcher/digging.cpp
+++ src/mineserver/watcher/digging.cpp
@@ -69,16 +69,5 @@ void Mineserver::Watcher_Digging::operator()(Mineserver::Game::pointer_t game, M
     response->type = 0;
     response->meta = 0;
     client->outgoing().push_back(response);
-
-		boost::shared_ptr<Mineserver::Network_Message_Chat> chatMessage = boost::make_shared<Mineserver::Network_Message_Chat>();
-		chatMessage->mid = 0x03;
-		chatMessage->message += "§4You broke the block at: ";
-		chatMessage->message += static_cast<int>(msg->x);
-		chatMessage->message += ",";
-		chatMessage->message += static_cast<int>(msg->y);
-		chatMessage->message += ",";
-		chatMessage->message += static_cast<int>(msg->z);
-		chatMessage->message += "!";
-		client->outgoing().push_back(chatMessage);
   }
 }
